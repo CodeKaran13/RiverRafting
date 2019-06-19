610a75e38906952285075d89290ff221d6fa19e7
Karan Nandkar <karan.nandkar@pocketaces.in>
2019-06-19 17:47:30 +0530

    level maps added

--- assets/RiverRafting/Scripts/Managers/MatchManager.ts
+++ assets/RiverRafting/Scripts/Managers/MatchManager.ts
@@ -3,11 +3,30 @@ const { ccclass, property } = cc._decorator;
 @ccclass
 export default class MatchManager extends cc.Component {
 
+    @property(cc.Node)
+    LevelPrefabs: cc.Node = null;
+
+    @property({
+        type: cc.Node,
+        visible: true,
+        serializable: true
+    })
+    RiverMapsNode: cc.Node = null;
+
     // onLoad () {}
 
     start() {
-
+        this.spawnNextRiverMap();
     }
 
     // update (dt) {}
+
+    spawnNextRiverMap() {
+        var nextMap = this.RiverMapsNode.children[0];
+        this.RiverMapsNode.removeChild(nextMap);
+         
+        this.LevelPrefabs.addChild(nextMap, 0, nextMap.name);
+        nextMap.setPosition(cc.Vec2.ZERO);
+        nextMap.active = true;
+    }
 }
