a76e4365eed3d8259cf6192835a0bbaa08c0e0b8
Karan Nandkar <karan.nandkar@pocketaces.in>
2019-06-19 13:53:12 +0530

    start

---
+++ assets/RiverRafting/Scripts/Managers/GameManager.ts
@@ -0,0 +1,13 @@
+const { ccclass, property } = cc._decorator;
+
+@ccclass
+export default class GameManager extends cc.Component {
+
+    // onLoad () {}
+
+    start() {
+
+    }
+
+    // update (dt) {}
+}
