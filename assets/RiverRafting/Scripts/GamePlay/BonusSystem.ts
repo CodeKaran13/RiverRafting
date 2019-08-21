62c1e4e495bacc82155c40329c714e281eeaefa2
Karan Nandkar <karan.nandkar@pocketaces.in>
2019-08-21 11:52:45 +0530

    clean run bonus changes done

--- assets/RiverRafting/Scripts/GamePlay/BonusSystem.ts
+++ assets/RiverRafting/Scripts/GamePlay/BonusSystem.ts
@@ -4,12 +4,11 @@ import GameManager, { GameState } from "../Managers/GameManager";
 const { ccclass, property } = cc._decorator;
 
 @ccclass
-export default class BonusSystem extends cc.Component
-{
+export default class BonusSystem extends cc.Component {
 
     currentTime: number = 0;
     @property
-    bonusTime: number = 5;
+    bonusStartTime: number = 5;
     @property
     bonusReward: number = 500;
     bonusMultiplier: number = 1;
@@ -17,48 +16,83 @@ export default class BonusSystem extends cc.Component
 
     // All script Refs
     _scoreManager: ScoreManager = null;
+    @property(cc.Label)
+    cleanRunTimeLabel: cc.Label = null;
 
-    resetBonus()
-    {
+    resetBonus() {
         this.bonusMultiplier = 1;
     }
-
-    restartCounter()
-    {
-        this.currentTime = this.bonusTime;
+    restartCounter() {
+        this.cleanTime = 0;
+        this.currentTime = this.bonusStartTime;
         this.startCounter();
     }
-
-    startCounter()
-    {
+    startCounter() {
         var time = cc.delayTime(1);
         this.sequence = cc.sequence(time, cc.callFunc(this.countdown, this));
         this.node.runAction(this.sequence.repeatForever());
     }
-
-    countdown()
-    {
+    countdown() {
         this.currentTime -= 1;
 
-        if (this.currentTime <= 0)
-        {
+        if (this.currentTime <= 0) {
             this.node.stopAction(this.sequence);
-            // reward player with clean run bonus and restart counter
+            // start rewarding player with clean run bonus until he hits the bound
 
-            this._scoreManager.AddBonus(this.bonusReward * this.bonusMultiplier);
-            this._scoreManager.AddScore(this.bonusReward * this.bonusMultiplier);
-            this.bonusMultiplier += 0.2;
-            if (GameManager.currentGameState == GameState.InGame)
-            {
-                this.restartCounter();
+            if (GameManager.currentGameState == GameState.InGame) {
+                // this.isCleanSequenceOn = false;
+                this.startCleanRunBonus();
             }
+            // this._scoreManager.AddBonus(this.bonusReward * this.bonusMultiplier);
+            // this._scoreManager.AddScore(this.bonusReward * this.bonusMultiplier);
+            // this.bonusMultiplier += 0.2;
+            // if (GameManager.currentGameState == GameState.InGame) {
+            //     this.restartCounter();
+            // }
         }
     }
 
-    stopAction()
-    {
+    stopAction() {
         this.node.stopAction(this.sequence);
+        this.node.stopAction(this.cleanRunSequence);
+        this.totalCleanTime = this.totalCleanTime + this.cleanTime;
+        console.log('total clean time in sec: ' + this.totalCleanTime);
+        this.closeLabel();
         this.resetBonus();
         this.restartCounter();
+    }
+
+    // isCleanSequenceOn: boolean = false;
+    totalCleanTime: number = 0;
+    cleanTime: number = 0;
+    cleanRunSequence: cc.ActionInterval;
+    startCleanRunBonus() {
+        // if (!this.isCleanSequenceOn) {
+            // this.isCleanSequenceOn = true;
+            console.log('started clean run');
+            var time = cc.delayTime(1);
+            this.cleanRunSequence = cc.sequence(time, cc.callFunc(this.cleanRunCountdown, this));
+            this.node.runAction(this.cleanRunSequence.repeatForever());
+        // }
+    }
+    cleanRunCountdown() {
+        this.cleanTime++;
+
+        this._scoreManager.AddBonus(this.bonusReward * this.bonusMultiplier);
+        this._scoreManager.AddScore(this.bonusReward * this.bonusMultiplier);
+        this.bonusMultiplier += 0.2;
+
+        this.onUIUpdate();
+    }
+
+    openLabel() {
+        this.cleanRunTimeLabel.node.active = true;
+    }
+    closeLabel() {
+        this.cleanRunTimeLabel.node.active = false;
+    }
+    onUIUpdate() {
+        this.openLabel();
+        this.cleanRunTimeLabel.string = 'CLEAN RUN: ' + this.cleanTime + 'SEC';
     }
 }
